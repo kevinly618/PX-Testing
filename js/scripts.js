569a9d49d46e498a48f93a71be84305fdd52b366
KL <kevinly618@gmail.com>
2020-05-19 16:32:19 -0500

    PX Script Publish May 19 4:31

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 /*alert('hello'); */
-
+<script type="text/javascript">
+  (function(n,t,a,e){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-9EQZKYSPO8MH-2");
+  
 function login(){
     //var a = document.form.usermail.value;
     var a = document.getElementById("usermail").value;
