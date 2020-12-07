04e8bb15c9551058bbe0be9b682c9e8eedd42fc5
kevinly618 <kevinly618@gmail.com>
2020-12-07 13:25:09 -0600

    Searchable Dropdown

--- js/scripts.js
+++ js/scripts.js
@@ -105,3 +105,26 @@ function loginB(){
 //     alert("enter kevinly618_QA@gmail.com or bsons_QA@gmail.com");
 //   return b;
 // }
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content https://www.w3schools.com/howto/howto_js_filter_dropdown.asp */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
